7b40af917e86b0d6d5a8b10b0dac0848cf09fc0f
Diogo Martins <emidemartins@proton.me>
2023-10-04 08:55:31 -0300

    Create products mock to response to API calls

---
+++ src/mocks/products.js
@@ -0,0 +1,19 @@
+const products = [
+  {
+    id: 1,
+    name: 'blue pen',
+    price: '1'
+  },
+  {
+    id: 2,
+    name: 'red pen',
+    price: '1'
+  },
+  {
+    id: 3,
+    name: 'black pen',
+    price: '1'
+  }
+]
+
+module.exports = products
