39a0006ea0f7a16214a6f58dd8b41d73feb47e5d
Diogo Martins <emidemartins@proton.me>
2023-10-05 10:57:06 -0300

    Sort products based on pquery params

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -2,8 +2,17 @@ const products = require('../mocks/products')
 
 const ProductController = {
   listProducts(request, response) {
+    const { order } = request.query
+    const sortedProducts = products.sort((a, b) => {
+      if (order === 'desc') {
+        return a.id < b.id ? 1 : -1
+      }
+
+      return a.id > b.id ? 1 : -1
+    })
+  
     response.writeHead(200, { 'Content-Type': 'application/json' })
-    response.end(JSON.stringify(products))
+    response.end(JSON.stringify(sortedProducts))
   }
 }
 
