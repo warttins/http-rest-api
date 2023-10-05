a94d012c9bcc390691adaf1d7e11513608f9d526
Diogo Martins <emidemartins@proton.me>
2023-10-05 10:54:25 -0300

    Create a way to filter query params from requested URL

--- src/server.js
+++ src/server.js
@@ -1,18 +1,21 @@
 const http = require('node:http')
+const { URL } = require('url')
 const routes = require('./routes')
 
 const server = http.createServer((request, response) => {
+  const parsedURL = new URL(`http://${request.headers.host}${request.url}`)
   console.log(`Request method: ${request.method} | Endpoint: ${request.url}`)
 
   const route = routes.find((routeObject) => (
-    routeObject.endpoint === request.url && routeObject.method === request.method
+    routeObject.endpoint === parsedURL.pathname && routeObject.method === request.method
   ))
 
   if (route) {
+    request.query = Object.fromEntries(parsedURL.searchParams)
     route.handler(request, response)
   } else {
     response.writeHead(404, { 'Content-Type': 'text/html' })
-    response.end(`Cannot ${request.method} ${request.url}`)
+    response.end(`Cannot ${request.method} ${parsedURL.pathname}`)
   }
 })
 
